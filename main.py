5587bb36fea115bbcd998234797496262a43a9a5
Jake Jaworski <jakejaworski28@gmail.com>
2022-06-01 18:03:02 -0700

    Added error handling

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ df = pd.DataFrame(raw_data)
 # convert data to dataframe
 nato_dict = {row["letter"]: row["code"] for (index, row) in df.iterrows()}
 
-# ask for user input and print the NATO translation
-user_input = input("Enter a word to be translated: ").upper()
-nato_letter_list = [nato_dict[letter] for letter in user_input]
-print(nato_letter_list)
+def generate_output():
+    # ask for user input and print the NATO translation
+    user_input = input("Enter a word to be translated: ").upper()
+    try:
+        nato_letter_list = [nato_dict[letter] for letter in user_input]
+        print(nato_letter_list)
+    except KeyError:
+        print("Sorry! Only alphabetical characters allowed!")
+        generate_output()
+
+generate_output()
+
 
 
 
